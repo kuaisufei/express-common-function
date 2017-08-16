74935e1cd12e1a11d80955b78175318379133049
zhangyuchao <2859941342@qq.com>
2017-08-16 17:39:49 +0800

    Signed-off-by: zhangyuchao <2859941342@qq.com> remove console.log

--- routes/mail.js
+++ routes/mail.js
@@ -3,9 +3,6 @@ const express = require('express');
 const router = express.Router();
 const mailService = require('../lib/mailService.js');
 
-console.log('mailService',mailService);
-
-console.log(mailService.sendmail);
 /* GET home page. */
 router.get('/send', function(req, res, next) {
 	mailService.sendmail({to: '2859941342@qq.com'});
